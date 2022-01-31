a3e361122dc86df01c5b3e2c37968f0bc411a2a5
Agata Bogacz <agatabogacz@outlook.com>
2022-01-31 16:48:35 +0100

    Heroku conf

--- src/server.js
+++ src/server.js
@@ -1,13 +1,7 @@
-const express = require("express");
-const app = express();
-const path = require("path");
-const PORT = 8000;
-
-app.use(express.static(path.join(__dirname, "client", "build")));
-app.get('/*', (req, res) => {
-   res.sendFile(path.join(__dirname, '../', 'client', 'build', 'index.html'));
-});
-
-app.listen(PORT, () => {
-   console.log(`Listening on port ${PORT}`);
-});
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+const app = express()
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+const port = process.env.PORT || 8080
+app.listen(port)
