03ae41b9eea989e104dbc43e319d21d1a0ec8623
Agata Bogacz <abogacz@akamai.com>
2022-02-01 16:20:04 +0100

    Updates

---
+++ precache-manifest.34a549170ad29c8f1fd68e954e887697.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94ed8c26b2c00604614be207026bb7ca",
+    "url": "/PortfolioReact/index.html"
+  },
+  {
+    "revision": "d844600ebd42be45295a",
+    "url": "/PortfolioReact/static/css/main.cc51f9da.chunk.css"
+  },
+  {
+    "revision": "fb46ec2e493b16614015",
+    "url": "/PortfolioReact/static/js/2.07b64c18.chunk.js"
+  },
+  {
+    "revision": "33490309eac47256b827830c802fd4a5",
+    "url": "/PortfolioReact/static/js/2.07b64c18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d844600ebd42be45295a",
+    "url": "/PortfolioReact/static/js/main.5c9070a6.chunk.js"
+  },
+  {
+    "revision": "f9f78fae8c86d7a5653f",
+    "url": "/PortfolioReact/static/js/runtime-main.e3c64e62.js"
+  },
+  {
+    "revision": "9ef4727a58325f432b790a1772992193",
+    "url": "/PortfolioReact/static/media/agh.9ef4727a.png"
+  },
+  {
+    "revision": "0c4aecfdb6048c4c323a03854a34de7e",
+    "url": "/PortfolioReact/static/media/akamai.0c4aecfd.png"
+  },
+  {
+    "revision": "856ceb3847a01a57da85ec9268e2bb7d",
+    "url": "/PortfolioReact/static/media/andea.856ceb38.png"
+  },
+  {
+    "revision": "881e6dca0ba211838662543166002248",
+    "url": "/PortfolioReact/static/media/andea.881e6dca.png"
+  },
+  {
+    "revision": "5f52755876350d4c89f7743763777271",
+    "url": "/PortfolioReact/static/media/bigcat.5f527558.gif"
+  },
+  {
+    "revision": "cf0f252823fb4e74f3ff8350be844564",
+    "url": "/PortfolioReact/static/media/chou.cf0f2528.jpg"
+  },
+  {
+    "revision": "5b54e5d4ebd7d599801f849d997c84e0",
+    "url": "/PortfolioReact/static/media/eevee.5b54e5d4.jpg"
+  },
+  {
+    "revision": "265b630ab01b56b8dfd24658cbca2e5d",
+    "url": "/PortfolioReact/static/media/gackt.265b630a.jpg"
+  },
+  {
+    "revision": "52a99cce5150559ce3c5eb6a6bd72c7e",
+    "url": "/PortfolioReact/static/media/gem.52a99cce.jpg"
+  },
+  {
+    "revision": "09193041a2b0d855baf72a7a3a64d455",
+    "url": "/PortfolioReact/static/media/hwa.09193041.jpg"
+  },
+  {
+    "revision": "16feb7d2dee9cb1b9f956d14422fac1e",
+    "url": "/PortfolioReact/static/media/jackie.16feb7d2.jpg"
+  },
+  {
+    "revision": "b2f3ccd0b9c5dddc18e595d2bdd5b288",
+    "url": "/PortfolioReact/static/media/jlpt.b2f3ccd0.png"
+  },
+  {
+    "revision": "e442bf9a0df255fc7d016c586e4e1c0e",
+    "url": "/PortfolioReact/static/media/me.e442bf9a.jpg"
+  },
+  {
+    "revision": "198b66b6256476b543f3e8d10cd3f7df",
+    "url": "/PortfolioReact/static/media/nangok.198b66b6.png"
+  },
+  {
+    "revision": "661c01bcdbb7b05adc6544cbf6f80ecf",
+    "url": "/PortfolioReact/static/media/nangoku.661c01bc.png"
+  },
+  {
+    "revision": "7498f20244f4b5907f3dc2cf56c06b10",
+    "url": "/PortfolioReact/static/media/nisemono1.7498f202.jpg"
+  },
+  {
+    "revision": "46633349786285667f62e0e062eb61dd",
+    "url": "/PortfolioReact/static/media/profile-mobile.46633349.jpg"
+  },
+  {
+    "revision": "ba23816f6f5675f2a58e0bdc714b28b4",
+    "url": "/PortfolioReact/static/media/profile5.ba23816f.jpg"
+  },
+  {
+    "revision": "7d156b9b6f10a335781add02bde4eb36",
+    "url": "/PortfolioReact/static/media/rei.7d156b9b.jpg"
+  },
+  {
+    "revision": "41763a046bc0485ff6c585eb9b570a35",
+    "url": "/PortfolioReact/static/media/reiwa.41763a04.jpg"
+  },
+  {
+    "revision": "e367c6828ce9aa2ca57393658a606095",
+    "url": "/PortfolioReact/static/media/ronbun.e367c682.jpg"
+  },
+  {
+    "revision": "3d691b26ed7a6381e56e9c75280fd8b7",
+    "url": "/PortfolioReact/static/media/sasaki.3d691b26.jpg"
+  },
+  {
+    "revision": "83cf2f6020e465ce175743d2229a0b80",
+    "url": "/PortfolioReact/static/media/suk.83cf2f60.jpg"
+  }
+]);
